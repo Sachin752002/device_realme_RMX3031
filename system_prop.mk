c8a01cbcb290550909d753740f6f481ed0f9a8d2
subhagamer <subhashissohail@gmail.com>
2022-06-10 22:48:31 +0530

    RMX3031: Add camera aux packagelist
    Signed-off-by: subhagamer <subhashissohail@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -77,3 +77,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 #AOD
 debug.force_no_blanking=true
+
+# Camera
+PRODUCT_SYSTEM_PROPERTIES += \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
+    vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord
